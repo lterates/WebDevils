0c2ffb5e42b249ac5b57ea199264a298f5973040
lterates <9180199@esmad.ipp.pt>
2021-05-04 21:57:31 +0100

    banner push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,13 @@
 <script>
 // @ is an alias to /src
 import NavBar from "@/components/navbar.vue";
+//import Banner from "@/components/Banner.vue";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    //Banner
   },
 
 
