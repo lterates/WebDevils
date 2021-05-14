56fb502985bdd02f9c87f4fb2d0259c3d32f6a5a
lterates <9180199@esmad.ipp.pt>
2021-05-14 14:38:47 +0100

    Footer Links updated and opening in new tab

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div style="position: fixed; bottom: 1rem; width: 100%">
         <span style="position: fixed; left: 10rem"><i>&copy; 2021 WebDevils</i></span>
-        <a href="https://facebook.com"><b-icon icon="facebook" style="margin-right: 10px;"></b-icon></a>
-        <a href="https://instagram.com"><b-icon icon="instagram" style="margin-right: 10px;"></b-icon></a>
-        <a href="https://twitter.com"><b-icon icon="twitter" style="margin-right: 10px;"></b-icon></a>
+        <a href="https://www.facebook.com/webdevilsofficial/" target="_blank"><b-icon icon="facebook" style="margin-right: 10px;"></b-icon></a>
+        <a href="https://www.instagram.com/webdevilsofficial/" target="_blank"><b-icon icon="instagram" style="margin-right: 10px;"></b-icon></a>
+        <a href="https://twitter.com/webdevilsesmad?s=20" target="_blank"><b-icon icon="twitter" style="margin-right: 10px;"></b-icon></a>
         <span style="position: fixed; right: 10rem"><i> Contact us at webdevils@protonmail.com</i></span>
     </div>
 </template>
