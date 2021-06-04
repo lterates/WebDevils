7605a406fa69505fe340a305a6f9bae858392408
JohnWink <luisbpires@hotmail.com>
2021-06-04 17:31:58 +0100

    add blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -28,33 +28,76 @@
             <b-container fluid>
                 <b-row>
                     <b-col>
-                        <h2>First Article</h2>
+                        <h2>How to self advertise</h2>
                     </b-col>  
                 </b-row>
                 <b-row>
                     <b-col>
-                        <p>This is the first article</p>
+                        <p>While there are plenty of people out there who love being the centre of attention, there are also those of us who prefer to stay out of the 
+                        limelight. Too much self-promotion could easily come across as bragging – and that’s not good for your reputation.
+                        The last thing you want is to start your social media profiles, begin establishing your personal brand, and discover that you’re coming off as a 
+                        bragging, pompous, idiot. The overwhelming threat of “making the wrong impression” can convince would-be entrepreneurs, creative minds, and freelancers 
+                        to avoid self-marketing entirely. However, this could mean that you miss out on valuable opportunities to boost your career.
+                        While speaking highly of yourself might feel uncomfortable, it serves an important purpose. After all, if you’re not willing to sing your praises, 
+                        who will be?
+                        Effective self-promotional strategies are all about finding the balance between demonstrating your skills and boasting about them. As difficult as it 
+                        can be to toe that line if you can get your promotional methods right, you could find that they open the door to promotions, new clients, and incredible 
+                        opportunities.
+                        When it comes to standing out in any marketplace, even individuals need to figure out how to successfully strut their stuff.
+                        </p>
                     </b-col>
                 </b-row>
                 <b-row>
                     <b-col>
-                        <h2>Second Article</h2>
+                        <h2>The importance of social media marketing</h2>
                     </b-col>
                 </b-row>
                 <b-row>
                     <b-col>
-                        <p>This is the second article</p>
+                        <p>
+                        Social media has become the most influential and important virtual space where the platform is not only used for social networking but is also a 
+                        great way of digitally advertising your brand and your products.
+                        Social media's power is commendable as you get to reach a large number of people within seconds of posting an ad, helping you reduce your costs, and 
+                        making your ads reach out to your potential audience through these social media advertisements.
+                        With the huge number of online users, which is almost 59% of the world’s population, marketers must not miss out on their chance of marketing on these 
+                        digital forums where they can reach all the maximum number of potential buyers compared with print or television media marketing.
+                        </p>
                     </b-col>
                 </b-row>
                 <b-row>
                     <b-col>
-                        <h2>Third Article</h2>
+                        <h2>Summer Blog Ideas</h2>
                     </b-col>
                     
                 </b-row>
                 <b-row>
                     <b-col>
-                        <p>This is the third article</p>
+                        <p>Can’t believe it’s already time to share our next season’s blog post idea list! Summer is always tough in blogging because who doesn’t want to 
+                            just be outside? We tried to make most of these ideas experienced-based, meaning that you actually have to go outside and *do* things in order to 
+                            write the blog post. It seems like a long process, but that results in really fun experiences and it also means that with each event comes not just 
+                            one quality blog post but often many. If you’re strategic about documenting your summer fun, you’ll be left with tons of engaging content for your 
+                            readers to enjoy! Onto the blog post ideas…
+
+                            1. Create a neighborhood guide for your fave hood in your city 
+                            2. Style a sunset shoot and turn it into an event to enjoy with your friends or partner.
+                            3. Look on your own actual bookshelf to create a summer reading list of old faves you want to revisit
+                            4. Attend an outdoor show or event and write a review or recap
+                            5. Take photos at a summer craft fair and promote some of the brands
+                            6. Share your editing process for editing natural light photos
+                            7. Make a craft for your next dinner party, like a cute table runner or drink stirrers
+                            8. Go on a solo walk or hike for some time to yourself and write your reflections
+                            9. Host a backyard cocktail party—have everyone bring an ingredient and experiment with new recipes
+                            10. Take a summer art class at a local arts center or college… you can write about this all quarter long!
+                            11. Find a local networking event and then share insights you gained from the experience
+                            12. Host a crafting in the park day where you get together with your local community to create. You can create a post recapping and one with a DIY tutorial
+                            13. Create a roundup of products based on your favorite summer colors
+                            14. Practice or experiment with golden hour photography and share the photos
+                            15. Create a summer fashion DIY and share the process on your blog
+                            16. Share some travel hacks that will make life easier for summer vacays
+                            17. Use downtime to write a post on a subject you don’t normally tackle, as inspired by our post on surprising readers
+                            18. Have a summer fun photo shoot featuring one of these trendy items or your own summer-themed pool and partyware
+                            19. Take photos during a trip and create something with them that you can do a blog tutorial on, like postcards or art
+                            </p>
                     </b-col>
                 </b-row>
             </b-container>
